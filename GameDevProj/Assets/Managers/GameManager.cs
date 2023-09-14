9749beb0262f13473c1d7d163ffb5608366cce15
Ryan Peebles <ryanjpeebles@gmail.com>
2023-09-14 17:05:51 -0400

    Just testing start enviroment for game

---
+++ GameDevProj/Assets/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+     public static GameManager instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+  
+}
+ public enum GameState{
+    SpawnUnits,
+    SpawnItems,
+    GameOver
+ }
