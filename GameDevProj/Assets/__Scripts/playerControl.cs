9cb0b071360614beab92fb4261d1448cd6d7cd12
FoxTrot2254 <elischomer@gmail.com>
2023-12-04 17:29:03 -0500

    updates

--- GameDevProj/Assets/__Scripts/playerControl.cs
+++ GameDevProj/Assets/__Scripts/playerControl.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerControl : basePlayer
 {
@@ -11,7 +13,15 @@ public class playerControl : basePlayer
     private bool pickupable = false;
     private GameObject item2;
     [SerializeField] public basePlayer Instance;
-
+    public movement stealthed;
+    public fov guardCheck;
+    public bool isSeen;
+    public GameObject[] stars;
+    public int interval = 5;
+    public Timer time;
+    public float initialTime;
+    public bool running = false;
+    int starNum = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,15 +29,33 @@ public class playerControl : basePlayer
         coll = GetComponent<BoxCollider2D>();
         text.text = "Gold: " + goldValue;
         Instance = this;
+        isSeen = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (guardCheck != null)
+        {
+            isSeen = guardCheck.playerSpotted;
+        }
+        else
+        {
+            isSeen = false;
+        }
         if (Input.GetKeyDown(KeyCode.E) && pickupable)
         {
             pickup();
         }
+        if (stealthed.Instance.stealth == stealth.stealthMode && isSeen && !running)
+        {
+            StarLevel();
+        }
+        else if (stealthed.Instance.stealth == stealth.StandMode && isSeen)
+        {
+
+        }
+
     }
 
     public void increaseGold()
@@ -63,4 +91,34 @@ public class playerControl : basePlayer
 */
     }
 
+    public void StarLevel()
+    {
+        StartCoroutine(Delay());
+        running = true;
+    }
+
+    IEnumerator Delay()
+    {
+        Debug.Log("start blinking");
+        StartCoroutine(Blink());
+        yield return new WaitForSeconds(.01f);
+        StopCoroutine(Blink());
+        stars[starNum].GetComponent<Image>().color = new Color32(255, 255, 225, 255);
+        starNum++;
+        Debug.Log("make next blink");
+        if (isSeen)
+        {
+            StartCoroutine(Delay());
+        }
+    }
+
+    IEnumerator Blink()
+    {
+        stars[starNum].GetComponent<Image>().color = new Color32(255, 255, 225, 255);
+        yield return new WaitForSeconds(0.2f);
+        stars[starNum].GetComponent<Image>().color = new Color32(0, 0, 0, 255);
+        yield return new WaitForSeconds(0.2f);
+
+
+    }
 }
